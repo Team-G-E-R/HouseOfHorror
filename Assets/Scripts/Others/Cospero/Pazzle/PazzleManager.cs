2d08e96a9758aebfb90a497e9d0087a4eee29f09
Zolotarev Evgenii Vladislavovich <xca0@yandex.ru>
2023-12-21 21:45:56 +0300

    Some edit

--- Assets/Scripts/Others/Cospero/Pazzle/PazzleManager.cs
+++ Assets/Scripts/Others/Cospero/Pazzle/PazzleManager.cs
@@ -1,10 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class PazzleManager : MonoBehaviour
 {
+    [SerializeField] private int TotalParts;
+    public GameObject OnWinActive;
     private bool _isMove;
     private Vector2 _mousePosition;
     private float _startPartPosX;
@@ -12,50 +11,50 @@ public class PazzleManager : MonoBehaviour
     private GameObject _pazzlePart;
     private bool _partFinish;
     private Vector3 _movingPatrFinishPos;
-    private int _solvedParts=0;
-    [SerializeField] int TotalParts;
-    public GameObject WinButton;
+    private int _solvedParts = 0;
 
-    private void Start() 
+    private void Start()
     {
-        WinButton.SetActive(false);
+        OnWinActive.SetActive(false);
     }
+
     public void StartMovingParts(GameObject part, Vector3 _finishPosition)
-    {   
-        _partFinish=false;
-        _movingPatrFinishPos=_finishPosition;
-        _pazzlePart=part;
-        _isMove=true;
-        _startPartPosX=_mousePosition.x-_pazzlePart.transform.localPosition.x;
-        _startPartPosY=_mousePosition.y-_pazzlePart.transform.localPosition.y;
-    } 
-    private void Update() 
     {
-        if(Input.GetKeyUp(KeyCode.Mouse0))
+        _partFinish = false;
+        _movingPatrFinishPos = _finishPosition;
+        _pazzlePart = part;
+        _isMove = true;
+        _startPartPosX = _mousePosition.x - _pazzlePart.transform.localPosition.x;
+        _startPartPosY = _mousePosition.y - _pazzlePart.transform.localPosition.y;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Mouse0) || _isMove == true)
         {
-            _isMove=false;
-            if(Mathf.Abs(_pazzlePart.transform.position.x -_movingPatrFinishPos.x)<=1f&&
-            (_pazzlePart.transform.position.y -_movingPatrFinishPos.y)<=1f)
+            _isMove = false;
+            Debug.Log(_solvedParts);
+
+            if(Mathf.Abs(_pazzlePart.transform.position.x -_movingPatrFinishPos.x) <= 1f &&
+            (_pazzlePart.transform.position.y -_movingPatrFinishPos.y) <= 1f)
             {
                 _pazzlePart.GetComponent<PolygonCollider2D>().enabled = false;
 
-                _pazzlePart.transform.position=new Vector3 (_movingPatrFinishPos.x,_movingPatrFinishPos.y,_movingPatrFinishPos.z);
+                _pazzlePart.transform.position = new Vector3 (_movingPatrFinishPos.x,
+                _movingPatrFinishPos.y,_movingPatrFinishPos.z);
 
-                _partFinish=true;
-                _solvedParts+=1;
-                if (_solvedParts==TotalParts)
-                {
-                    WinButton.SetActive(true);
-                }
-                Debug.Log(_movingPatrFinishPos);
+                _partFinish = true;
+                _solvedParts += 1;
+                if (_solvedParts == TotalParts)
+                    OnWinActive.SetActive(true);
             }
         }
-        _mousePosition=Input.mousePosition;
-        if(_isMove&& !_partFinish)
-        {
-            _pazzlePart.transform.localPosition=new Vector3(_mousePosition.x-_startPartPosX,_mousePosition.y-_startPartPosY,0);
-            Debug.Log(_mousePosition);
-        }
+
+        _mousePosition = Input.mousePosition;
+
+        if(_isMove && !_partFinish)
+            _pazzlePart.transform.localPosition = new Vector3(_mousePosition.x - _startPartPosX,
+            _mousePosition.y - _startPartPosY,0);
     }
     
 }
--- Assets/Scripts/Others/Cospero/Pazzle/PazzlePartsMove.cs
+++ Assets/Scripts/Others/Cospero/Pazzle/PazzlePartsMove.cs
@@ -1,26 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using Common.Scripts;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PazzlePartsMove : MonoBehaviour
 {
-    private float _startPartPosX;
-    private float _startPartPosY;
     public GameObject FinishOb;
     private Vector3 _finishPosition;
     
     private void OnMouseDown() 
     {
-        if(Input.GetMouseButtonDown(0)&&(this.gameObject.transform.position!=FinishOb.transform.position))
+        if(Input.GetMouseButtonDown(0) && (this.gameObject.transform.position != 
+        FinishOb.transform.position))
         {
-            _finishPosition=new Vector3(FinishOb.transform.position.x,FinishOb.transform.position.y,FinishOb.transform.position.z);
-            Debug.Log(_finishPosition);
+            _finishPosition = new Vector3(FinishOb.transform.position.x,
+            FinishOb.transform.position.y, FinishOb.transform.position.z);
             FindObjectOfType<PazzleManager>().StartMovingParts(this.gameObject, _finishPosition);
         }
     }
-
-   
 }
