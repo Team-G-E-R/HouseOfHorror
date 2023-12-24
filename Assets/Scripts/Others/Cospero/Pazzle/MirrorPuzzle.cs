b38f63ae93e07d93ed574766ac058f566d88a1b4
Zikkey <68398139+ZikkeyLS@users.noreply.github.com>
2023-12-24 22:41:22 +0300

    Mirror fix (#46)

---
+++ Assets/Scripts/Others/Cospero/Pazzle/MirrorPuzzle.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum PartNumber
+{
+    First = 0,
+    Second,
+    Third,
+    Fourth,
+    Fifth,
+    Six
+}
+
+public class MirrorPuzzle : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _onWin;
+    [SerializeField] private float _maxPositionDifferences = 10f;
+    [SerializeField] private MirrorDragAndDrop[] _dynamicParts;
+    [SerializeField] private MirrorConstant[] _constantParts;
+
+    public static MirrorPuzzle Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void CheckRelative(MirrorDragAndDrop mirrorDragAndDrop)
+    {
+        foreach (MirrorConstant constantPart in _constantParts)
+            if (mirrorDragAndDrop.GetPartNumber() == constantPart.GetPartNumber() 
+                && _maxPositionDifferences >= Vector3.Distance(mirrorDragAndDrop.transform.position, constantPart.transform.position))
+            {
+                constantPart.Completed = true;
+                mirrorDragAndDrop.transform.position = constantPart.transform.position;
+                mirrorDragAndDrop.SetLocked(true);
+            }
+
+        CheckWin();
+    }
+
+    public void CheckWin()
+    {
+        bool win = true;
+
+        foreach (MirrorConstant constantPart in _constantParts)
+            if (constantPart.Completed == false)
+                win = false;
+
+        if (win)
+            _onWin.Invoke();
+    }
+}
