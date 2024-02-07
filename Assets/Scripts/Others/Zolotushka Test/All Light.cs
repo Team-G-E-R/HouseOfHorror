17348a859e0dab95fcb8ec4fe822c53a2b1126fd
Zolotarev Evgenii Vladislavovich <xca0@yandex.ru>
2024-02-07 21:39:30 +0300

    Special vision update

--- Assets/Scripts/Others/Zolotushka Test/All Light.cs
+++ Assets/Scripts/Others/Zolotushka Test/All Light.cs
@@ -1,30 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class AllLight : MonoBehaviour
 {
-    [SerializeField] private Light[] SpecialLights;
+    [SerializeField] private Light[] _specialLights;
+    [Tooltip("Recomended 1")]
+    [SerializeField] private float _chromaticIntensity;
+    [Tooltip("Recomended 60")]
+    [SerializeField] private float _lensIntensity;
+    [SerializeField] private bool _needToShowHidden;
+    [SerializeField] private GameObject[] _hiddenObjs;
     private Light[] _lights;
+    private PostProcessVolume _postProcess;
+    private float _standartChromatic;
+    private float _standartLens;
+    private bool state;
+    private bool _hidden;
 
-    void Update()
+    private void OnEnable()
     {
-        if (Input.GetKey(KeyCode.Tab))
+        _postProcess = GameObject.
+        FindGameObjectWithTag("PostProcessing").GetComponent<PostProcessVolume>();
+        _standartChromatic = _postProcess.profile.GetSetting<ChromaticAberration>().intensity.value;
+        _standartLens = _postProcess.profile.GetSetting<LensDistortion>().intensity.value;
+
+        state = false;
+        _hidden = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
-            SwitchLight();
+            state = !state;
+            _hidden = !_hidden;
+            SwitchLight(state);
         }
     }
 
-    public void SwitchLight()
+    private void SwitchLight(bool isState)
     {
         _lights = FindObjectsOfType<Light>();
         foreach (var light in _lights)
         {
-            light.enabled = false;
+            light.enabled = !isState;
         }
-        foreach (var lightS in SpecialLights)
+        foreach (var lightS in _specialLights)
         {
-            lightS.enabled = true;
+            lightS.enabled = isState;
+        }
+        if (isState == true)
+        {
+            _postProcess.profile.GetSetting<ChromaticAberration>().intensity.value = _chromaticIntensity;
+            _postProcess.profile.GetSetting<LensDistortion>().intensity.value = _lensIntensity;
+        }
+        else
+        {
+            _postProcess.profile.GetSetting<ChromaticAberration>().intensity.value = _standartChromatic;
+            _postProcess.profile.GetSetting<LensDistortion>().intensity.value = _standartLens;
+        }
+        if (_hiddenObjs.Length != 0)
+        {
+            foreach (var obj in _hiddenObjs)
+            {
+                obj.SetActive(_hidden);
+            }   
         }
     }
 }
