082868afd91744608b8a0666cd55e9cb257bd067
Zolotarev Evgenii Vladislavovich <xca0@yandex.ru>
2024-03-03 03:22:09 +0300

    Camera rework done (need to change objs)

--- Assets/Scripts/Common/Player/CameraZone.cs
+++ Assets/Scripts/Common/Player/CameraZone.cs
@@ -5,57 +5,48 @@ public class CameraZone : MonoBehaviour
 {
     [Tooltip ("FOV when entering the trigger")]
     [SerializeField] private float _fovEnd;
-    [Tooltip ("Recommended to use around 10")]
+    [Tooltip ("Recommended to use around 0.001")]
     [SerializeField] private float _timeToChangeFov;
+    [Tooltip ("Recommended to use around 0.05")]
     [SerializeField] private float _valueToChangeFovBack;
+    [Tooltip ("Recommended to use around 0.05")]
     [SerializeField] private float _valueToChangeFovEnter;
-    
-    private bool _isLerp = true;
+    [SerializeField] private bool _needToChangeRotation;
+    [SerializeField] private Vector3 _valueToFinalRotation;
+    [Tooltip ("Recommended to use around 0.003-0.005")]
+    [SerializeField] private float _timeToChangeRotation;
+
     private Camera _camera;
-    private float _cameraFovEnter;
-    private CameraZone[] _allObjs;
+    private static float _cameraFovEnter;
+    private Quaternion _cameraRotationStart;
+    private Quaternion _valueToFinalRotationQuaternion;
 
     private void Awake()
     {
         _camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         _cameraFovEnter = _camera.fieldOfView;
-        _allObjs = FindObjectsOfType<CameraZone>();
-    }
-
-    private void Update()
-    {
-        
+        _cameraRotationStart = _camera.transform.rotation;
+        _valueToFinalRotationQuaternion = Quaternion.
+        Euler(_valueToFinalRotation.x, _valueToFinalRotation.y, _valueToFinalRotation.z);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            StopAllCoroutines();
+            StartCoroutine(CameraRotation(true));
             StartCoroutine(CameraFOVEnter());
-            foreach (var a in _allObjs)
-            {
-                a._isLerp = false;
-            }   
         }
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            //_camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, this._fovEnd, _timeToChangeFov);
-        }
-    }
-    
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
         {
+            StopAllCoroutines();
+            StartCoroutine(CameraRotation(false));
             StartCoroutine(CameraFOVBack());
-            foreach (var a in _allObjs)
-            {
-                a._isLerp = true;
-            }  
         }
     }
 
@@ -65,7 +56,6 @@ public class CameraZone : MonoBehaviour
         {
             while (_camera.fieldOfView >= _cameraFovEnter)
             {
-                print(gameObject.name + " out");
                 _camera.fieldOfView -= _valueToChangeFovBack;
                 yield return new WaitForSeconds(_timeToChangeFov);
             }
@@ -74,7 +64,6 @@ public class CameraZone : MonoBehaviour
         {
             while (_camera.fieldOfView <= _cameraFovEnter)
             {
-                print(gameObject.name + " out");
                 _camera.fieldOfView += _valueToChangeFovBack;
                 yield return new WaitForSeconds(_timeToChangeFov);
             }
@@ -83,27 +72,45 @@ public class CameraZone : MonoBehaviour
 
     private IEnumerator CameraFOVEnter()
     {
-        print(_cameraFovEnter);
-        print(_fovEnd);
         if (_cameraFovEnter < _fovEnd)
         {
-            print("in if");
-            while (_camera.fieldOfView >= _fovEnd)
+            while (_camera.fieldOfView <= _fovEnd)
             {
-                print(gameObject.name + " entered");
-                _camera.fieldOfView -= _valueToChangeFovEnter;
+                _camera.fieldOfView += _valueToChangeFovEnter;
                 yield return new WaitForSeconds(_timeToChangeFov);
             }
         }
         else if (_cameraFovEnter > _fovEnd)
         {
-            print("in if 2");
-            while (_camera.fieldOfView <= _fovEnd)
+            while (_camera.fieldOfView >= _fovEnd)
             {
-                print(gameObject.name + " entered");
                 _camera.fieldOfView -= _valueToChangeFovEnter;
                 yield return new WaitForSeconds(_timeToChangeFov);
             }
+        }
+    }
+
+    private IEnumerator CameraRotation(bool entered)
+    {
+        if (entered)
+        {
+            while (_camera.transform.rotation != _valueToFinalRotationQuaternion)
+            {
+                 _camera.transform.rotation = Quaternion.
+                    Slerp(_camera.transform.rotation,
+                        _valueToFinalRotationQuaternion, _timeToChangeRotation);
+                yield return null;
+            }
+        }
+        else if (!entered)
+        {
+            while (_camera.transform.rotation != _cameraRotationStart)
+            {
+                _camera.transform.rotation = Quaternion.
+                    Slerp(_camera.transform.rotation, 
+                        _cameraRotationStart, _timeToChangeRotation);
+                yield return null;
+            }   
         }
     }
 }
